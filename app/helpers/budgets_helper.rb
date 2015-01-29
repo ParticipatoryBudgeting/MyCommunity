2fa48f1576c8661c733a05757a4ff791c165f6e7
Mateusz Baltyn <mateusz.baltyn@gmail.com>
2015-01-29 15:02:53 +0100

    Uzupełnienie danych w widoku budżetu oraz projektu

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -1,2 +1,10 @@
 module BudgetsHelper
+
+  def display_pre_days(budget)
+    if not budget.preparation_date.nil? and not budget.creation_date.nil?
+      (budget.preparation_date - budget.creation_date).days
+    else
+      ''
+    end
+  end
 end
